845ad77bb307b46c620ebb63797e90e882f585db
Sasaki Toru <sasakitoa@nttdata.co.jp>
2016-08-14 12:03:26 +0900

    Fixed bug in RandomInt when occurred min.key and max.key are same.

--- src/main/java/sasakitoa/kafka/connect/random/generator/RandomInt.java
+++ src/main/java/sasakitoa/kafka/connect/random/generator/RandomInt.java
@@ -94,7 +94,11 @@ public class RandomInt extends Generator {
     }
 
     private int getRandomValue(int min, int max) {
-        long value = (long)rand.nextInt();
-        return (int)((value - (long)(Integer.MIN_VALUE)) % (max - min) + min);
+        if(min == max) {
+            return min;
+        } else {
+            long value = (long) rand.nextInt();
+            return (int) ((value - (long) (Integer.MIN_VALUE)) % (max - min) + min);
+        }
     }
 }
